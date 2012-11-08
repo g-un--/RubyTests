9646e29a5e995908386ee43432c60e66f83de92f
g-un-- <office.gino@gmail.com>
2012-11-08 00:26:55 +0000

    RubyMonk 0.2 Syntactic Sugar for Special Methods

---
+++ RubyMonk/0.2.rb
@@ -0,0 +1,9 @@
+puts "4+3 ->", 4+3
+puts
+words=['foo', 'bar', 'baz']
+puts "words=['foo', 'bar', 'baz'] ->", words
+puts
+puts "words[1] ->", words[1] 
+puts
+puts "words.[](1) ->", words.[](1)
+puts
