279c5e6d61c5c9fa7e42c477ad819c50d01e71e5
g-un-- <office.gino@gmail.com>
2012-11-08 21:50:35 +0000

    RubyMonk 2.0 Boolean Expressions in Ruby

---
+++ RubyMonk/2.0.rb
@@ -0,0 +1,20 @@
+name = 'monk'
+
+puts "name = 'monk'"
+puts "name == 'monk' ->"
+puts name == 'monk'
+puts
+
+age = 24
+puts 'age = 24'
+puts 'age < 35 ->'
+puts age < 35
+puts 
+
+puts "age > 23 && (name == 'monk' || name == 'gino')"
+puts  age > 23 && (name == 'monk' || name == 'gino')
+puts
+
+puts "!(name == 'Bob')"
+puts !(name == 'Bob') 
+puts
