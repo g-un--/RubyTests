d39d7b3b35b283332f86c4b4fd853a9d77bee5ba
g-un-- <office.gino@gmail.com>
2012-11-08 23:24:51 +0000

    RubyMonk 5.1 Building your own class

---
+++ RubyMonk/5.1.rb
@@ -0,0 +1,18 @@
+rectangle = 
+<<-eos
+class Rectangle
+  def initialize(length, breadth)
+    @length = length
+    @breadth = breadth
+  end
+
+  def perimeter
+    2 * (@length + @breadth)
+  end
+
+  def area
+    @length * @breadth
+  end
+end
+eos
+puts rectangle
