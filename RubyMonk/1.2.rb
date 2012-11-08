7937e308db862da11f7816b6977f310e1a8e3df2
g-un-- <office.gino@gmail.com>
2012-11-08 21:40:01 +0000

    RubyMonk 1.2 Advanced String Operations

---
+++ RubyMonk/1.2.rb
@@ -0,0 +1,7 @@
+puts "'Fear is the path to the dark side'.split(' ')"
+puts 'Fear is the path to the dark side'.split(' ')
+puts
+
+puts "'RubyMonk Is Pretty Brilliant'.gsub(/[A-Z]/,'0')"
+puts 'RubyMonk Is Pretty Brilliant'.gsub(/[A-Z]/,'0')
+puts
