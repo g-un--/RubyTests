9565f46322ca727908e24b173023bcaf983d88e4
g-un-- <office.gino@gmail.com>
2012-11-24 23:47:52 +0000

    Added to_s method to Song class

--- ProgrammingRuby/Song.rb
+++ ProgrammingRuby/Song.rb
@@ -4,7 +4,12 @@ class Song
     @artist = artist
     @duration = duration
   end
+  
+  def to_s
+    puts "#{@name}--#{@artist} #{@duration}"
+  end
 end
 
 song = Song.new("Paradise Circus", "Massive Attack", 315)
 puts song.inspect
+puts song.to_s
