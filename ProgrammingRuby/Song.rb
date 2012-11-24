537baff79f5d7dc33188fd6bee7d3a3e74114890
g-un-- <office.gino@gmail.com>
2012-11-24 23:58:17 +0000

    Song attr_reader, attr_writer or property=(value)

--- ProgrammingRuby/Song.rb
+++ ProgrammingRuby/Song.rb
@@ -1,10 +1,16 @@
 class Song
+  attr_reader :name, :artist, :duration
+  
   def initialize(name, artist, duration)
     @name = name
     @artist = artist
     @duration = duration
   end
   
+  def duration=(newDuration)
+    @duration = newDuration
+  end
+
   def to_s
     puts "#{@name}--#{@artist} #{@duration}"
   end
@@ -13,3 +19,5 @@ end
 song = Song.new("Paradise Circus", "Massive Attack", 315)
 puts song.inspect
 puts song.to_s
+song.duration = 320
+puts song.duration
